fa19876c411d3af3bfdc971e8ab135f4c2289c17
takuyaw-w <takuya.jamtb@gmail.com>
2021-06-07 23:26:14 +0900

    hooks useState

---
+++ try_vite/vite-react-hooks/src/App.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import './App.css'
+
+const App: React.FC = () => {
+  const initialState: number = Math.floor(Math.random() * 10) + 1
+  const [count, setCount] = useState<number>(initialState)
+  const [open, setOpen] = useState<boolean>(true)
+  const toggle = () => setOpen(!open)
+
+  return (
+    <>
+      <button onClick={toggle}>{open ? 'close' : 'open'}</button>
+      <div className={open ? 'isOpen' : 'isClose'}>
+        <p>{count}</p>
+        <button onClick={() => setCount(prevState => prevState + 1)}>+1</button>
+        <button onClick={() => setCount(count - 1)}>-1</button>
+        <button onClick={() => setCount(0)}>0</button>
+        <button onClick={() => setCount(initialState)}>initialize</button>
+      </div>
+    </>
+  )
+}
+
+export default App
