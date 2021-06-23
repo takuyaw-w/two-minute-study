ae1b524999322d58da5a520dc414a0af66150ae1
takuyaw-w <takuya.jamtb@gmail.com>
2021-06-24 01:26:49 +0900

    wip

---
+++ try_vue3_todo/src/views/todos.vue
@@ -0,0 +1,26 @@
+<template>
+  <h2>TODO一覧</h2>
+  <ul>
+    <li v-for="todo in todoStore.state.todos" :key="todo.id">
+      {{ todo.title }}
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent, inject } from 'vue'
+import { key } from '@/store/todo'
+
+export default defineComponent({
+  setup() {
+    const todoStore = inject(key)
+    if (!todoStore) {
+      throw new Error('todoStore is not provided')
+    }
+
+    return {
+      todoStore
+    }
+  },
+})
+</script>
