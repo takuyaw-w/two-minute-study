4c3cc1bdf2585a287b8dd572fcff329b1ab0c76a
takuyaw-w <takuya.jamtb@gmail.com>
2021-06-24 01:26:53 +0900

    wip

--- try_vue3_todo/src/router/index.ts
+++ try_vue3_todo/src/router/index.ts
@@ -1,6 +1,12 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import Todos from '@/views/todos.vue'
 
 const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'Todos',
+    component: Todos
+  }
 ]
 
 const router = createRouter({
