3cde108cbe81cfb21d673ff4fde910e860e2d21e
takuyaw-w <takuya.jamtb@gmail.com>
2021-06-24 02:16:51 +0900

    wip

--- try_vue3_todo/src/views/todos.vue
+++ try_vue3_todo/src/views/todos.vue
@@ -1,9 +1,13 @@
 <template>
   <h2>TODO一覧</h2>
   <ul>
-    <li v-for="todo in todoStore.state.todos" :key="todo.id">
-      {{ todo.title }}
-    </li>
+    <todo-item
+      v-for="todo in todoStore.state.todos"
+      :key="todo.id"
+      :todo="todo"
+      @click-delete="clickDelete"
+      @click-title="clickTitle"
+    ></todo-item>
   </ul>
   <router-link to="/new">新規作成</router-link>
 </template>
@@ -11,17 +15,34 @@
 <script lang="ts">
 import { defineComponent, inject } from 'vue'
 import { key } from '@/store/todo'
+import TodoItem from '@/components/TodoItem.vue'
+import { useRouter } from 'vue-router'
 
 export default defineComponent({
+  components: {
+    TodoItem
+  },
   setup() {
     const todoStore = inject(key)
     if (!todoStore) {
       throw new Error('todoStore is not provided')
     }
 
-    return {
-      todoStore
+    const router = useRouter()
+
+    const clickDelete = (id: number) => {
+      todoStore.deleteTodo(id)
     }
-  },
+
+    const clickTitle = (id: number) => {
+      router.push(`/edit/${id}`)
+    }
+
+    return {
+      todoStore,
+      clickDelete,
+      clickTitle
+    }
+  }
 })
 </script>
