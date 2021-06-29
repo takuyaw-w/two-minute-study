2175cc2fc83d8a19caadfb7e9d724daa4593295e
takuyaw-w <takuya.jamtb@gmail.com>
2021-06-30 00:42:20 +0900

    update

--- try_vue3_list2tree/list2tree.js
+++ try_vue3_list2tree/list2tree.js
@@ -1,1 +1,24 @@
-console.log(Vue.version)
+Vue.createApp({
+  data() {
+    return {
+      items: [
+        { id: 0, value: "item-0", indent: 0},
+        { id: 1, value: "item-1", indent: 1},
+        { id: 2, value: "item-2", indent: 1},
+      ]
+    }
+  },
+  methods: {
+    onInput(e, item) {
+      item.value = e.target.innerText
+    },
+    onTab(item, index) {
+      item.indent++
+    }
+  },
+  computed: {
+    tree() {
+      return this.items.map(item => item.value + item.indent).join('\n')
+    }
+  }
+}).mount('#app')
