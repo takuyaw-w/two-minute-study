8136e5157c5a4f6e84b70454d74ea100855c22c4
takuyaw-w <takuya.jamtb@gmail.com>
2021-08-03 21:44:29 +0900

    update

--- try_nuxt_vuetify/pages/index.vue
+++ try_nuxt_vuetify/pages/index.vue
@@ -1,25 +1,9 @@
 <template>
   <v-row justify="center" align="center">
-    <v-data-table
-      :headers="headers"
-      :items="items"
-    >
-      <template v-slot:body="{ items }">
-        <tbody>
-          <tr v-for="item in items" :key="item.id">
-            <td>
-              {{ item.name }}<br />
-              {{ item.calories }}
-            </td>
-            <td>{{ item.calories }}</td>
-            <td>{{ item.fat }}</td>
-            <td>{{ item.carbs }}</td>
-            <td>{{ item.protein }}</td>
-            <td>{{ item.iron }}</td>
-          </tr>
-        </tbody>
-      </template>
-    </v-data-table>
+    <v-date-picker
+      v-model="picker"
+      :allowed-dates="allowed"
+    ></v-date-picker>
   </v-row>
 </template>
 
@@ -27,106 +11,19 @@
 export default {
   data() {
     return {
-      headers: [
-        { text: 'Dessert (100g serving)', value: 'name',},
-        { text: 'Calories', value: 'calories' },
-        { text: 'Fat (g)', value: 'fat' },
-        { text: 'Carbs (g)', value: 'carbs' },
-        { text: 'Protein (g)', value: 'protein' },
-        { text: 'Iron (%)', value: 'iron' },
-      ],
-      items: [
-        {
-          id: 1,
-          name: 'Frozen Yogurt',
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: '1%',
-        },
-        {
-          id: 2,
-          name: 'Ice cream sandwich',
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: '1%',
-        },
-        {
-          id: 3,
-          name: 'Eclair',
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: '7%',
-        },
-        {
-          id: 4,
-          name: 'Cupcake',
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: '8%',
-        },
-        {
-          id: 5,
-          name: 'Gingerbread',
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: '16%',
-        },
-        {
-          id: 6,
-          name: 'Jelly bean',
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: '0%',
-        },
-        {
-          id: 7,
-          name: 'Lollipop',
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: '2%',
-        },
-        {
-          id: 8,
-          name: 'Honeycomb',
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: '45%',
-        },
-        {
-          id: 9,
-          name: 'Donut',
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: '22%',
-        },
-        {
-          id: 10,
-          name: 'KitKat',
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: '6%',
-        },
-      ],
+      picker: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+    }
+  },
+  methods: {
+    allowed(val) {
+      let today = new Date("2020-05-01")
+      today = new Date(
+        today.getFullYear(),
+        today.getMonth(),
+        today.getDate()
+      )
+
+      return today < new Date(val)
     }
   }
 }
