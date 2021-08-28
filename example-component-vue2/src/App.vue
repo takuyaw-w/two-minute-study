2fede5abc19a2c403cded50ec2d300b7f9637f60
takuyaw-w <takuya.jamtb@gmail.com>
2021-08-28 21:48:55 +0900

    wip

--- example-component-vue2/src/App.vue
+++ example-component-vue2/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <my-input v-model="val" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MyInput from "./components/MyInput.vue";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyInput
+  },
+  data() {
+    return {
+      val: ""
+    }
   }
 }
 </script>
---
+++ example-component-vue2/src/components/MyInput.vue
@@ -0,0 +1,77 @@
+<script>
+const INPUT_TYPES = {
+  TEXT: "text",
+  NUMBER: "number"
+}
+const EVENT_TYPES = {
+  FOCUS: "focus",
+  BLUR: "blur"
+}
+const DISALLOW_KEYS = [
+  "Minus", // マイナス（ハイフン）
+  "KeyE", // HACK: Chromeではinput type="number"に指数を表すeが入力できてしまうため設定する
+  "Semicolon", // プラス
+  "Period", // ドット
+]
+
+export default {
+  name: "MyInput",
+  props: {
+    value: {
+      type: [String, Number]
+    }
+  },
+  data() {
+    return {
+      inputType: "text",
+      editableValue: this.value,
+    }
+  },
+  methods: {
+    changeInputType(e) {
+      if (e.type === EVENT_TYPES.FOCUS) {
+        this.editableValue = this.editableValue.replaceAll(",", "")
+        this.inputType = INPUT_TYPES.NUMBER
+      } else if (e.type === EVENT_TYPES.BLUR) {
+        this.inputType = INPUT_TYPES.TEXT
+        this.editableValue = new Intl.NumberFormat("ja-JP").format(this.editableValue)
+      }
+    },
+    onInput(e) {
+      this.editableValue = e.target.value
+      this.$emit("input", this.editableValue)
+    },
+    onKeydown(e) {
+      const code = e.code
+      const isDisallowKey = DISALLOW_KEYS.some(key => key === code)
+      if (isDisallowKey) {
+        e.preventDefault()
+      }
+    }
+  },
+}
+</script>
+
+<style scoped>
+input {
+  font-size: 32px;
+  padding: 10px;
+  text-align: right;
+}
+input[type="number"]::-webkit-outer-spin-button, 
+input[type="number"]::-webkit-inner-spin-button { 
+  -webkit-appearance: none; 
+  margin: 0; 
+} 
+</style>
+
+<template>
+  <input
+    :value="editableValue"
+    @input="onInput"
+    :type="inputType"
+    @focus="changeInputType"
+    @blur="changeInputType"
+    @keydown="onKeydown"
+  >
+</template>
